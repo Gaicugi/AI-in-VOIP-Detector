ea4efd64291b6fe51866fca1c1123ab40591f4ec
Gaicugi <95116161+Gaicugi@users.noreply.github.com>
2026-03-27 22:39:23 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+"""Shim setup file to allow for editable install."""
+
+from setuptools import setup
+
+scripts=[
+  "bin/sippts",
+  "bin/sippts-gui"
+]
+
+if __name__ == "__main__":
+  setup(scripts=scripts,
+    name='SIPPTS',
+      author='Jose Luis Verdeguer aka Pepelux',
+      version='4.1',
+      install_requires=[     
+        'netifaces',
+        'requests',
+        'IPy',
+        'scapy',
+        'pyshark',
+        'websocket-client',
+        'rel',
+        'resource'
+      ],
+      extra_requires=[
+        'cursor',
+        'ArpSpoof',
+        'asterisk-ami',
+        'Cmd'
+      ],
+    include_package_data=True,
+    package_data={'sippts': ['data/cve.csv']}
+    )
